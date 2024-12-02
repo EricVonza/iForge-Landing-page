8f530922d67f037832e876ae3c43b662f559f583
Eric Vonza <ericvonza@localhost.localdomain>
2024-12-02 20:52:39 +0300

    Added New SVG Icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path from "path";
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), themePlugin()],
+  plugins: [react(), themePlugin(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
