3409d5f12382337b49fe3d8cb8bac528fb47ea23
Eric <ericvonza47@gmail.com>
2024-12-11 16:28:58 +0300

    Added dist bundle

---
+++ dist/index.js
@@ -0,0 +1,86 @@
+// server/index.ts
+import express2 from "express";
+
+// server/routes.ts
+function registerRoutes(app2) {
+}
+
+// server/vite.ts
+import express from "express";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { createServer as createViteServer } from "vite";
+var __filename = fileURLToPath(import.meta.url);
+var __dirname = dirname(__filename);
+async function setupVite(app2, server) {
+  const vite = await createViteServer({
+    server: {
+      middlewareMode: true,
+      hmr: { server }
+    },
+    clearScreen: false,
+    appType: "custom"
+  });
+  app2.use(vite.middlewares);
+  app2.use("*", async (req, res, next) => {
+    const url = req.originalUrl;
+    try {
+      const clientTemplate = path.resolve(
+        __dirname,
+        "..",
+        "client",
+        "index.html"
+      );
+      let template = await fs.promises.readFile(clientTemplate, "utf-8");
+      template = await vite.transformIndexHtml(url, template);
+      res.status(200).set({ "Content-Type": "text/html" }).end(template);
+    } catch (e) {
+      vite.ssrFixStacktrace(e);
+      next(e);
+    }
+  });
+}
+function serveStatic(app2) {
+  const distPath = path.resolve(__dirname, "public");
+  if (!fs.existsSync(distPath)) {
+    throw new Error(
+      `Could not find the build directory: ${distPath}, make sure to build the client first`
+    );
+  }
+  app2.use(express.static(distPath));
+  app2.use("*", (_req, res) => {
+    res.sendFile(path.resolve(distPath, "index.html"));
+  });
+}
+
+// server/index.ts
+import { createServer } from "http";
+var app = express2();
+app.use(express2.json());
+app.use(express2.urlencoded({ extended: false }));
+(async () => {
+  registerRoutes(app);
+  const server = createServer(app);
+  app.use((err, _req, res, _next) => {
+    const status = err.status || err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(status).json({ message });
+    throw err;
+  });
+  if (app.get("env") === "development") {
+    await setupVite(app, server);
+  } else {
+    serveStatic(app);
+  }
+  const PORT = 5e3;
+  server.listen(PORT, "0.0.0.0", () => {
+    const formattedTime = (/* @__PURE__ */ new Date()).toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true
+    });
+    console.log(`${formattedTime} [express] serving on port ${PORT}`);
+  });
+})();
